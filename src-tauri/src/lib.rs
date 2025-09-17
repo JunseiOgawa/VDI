1e1e1abe5a57a14ae044551ac28de25bf99d3d63
JUNSEI <junsei2005@outlook.jp>
2025-09-17 10:32:20 +0900

    【add】引数に画像を渡すと画像表示する機能の実装

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,11 +4,20 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+
+//起動引数から１つ目を返すコマンド
+
+#[tauri::command]
+fn get_launch_image_path() -> Option<String> {
+    std::env::args().nth(1)
+}
+
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_launch_image_path])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,27 @@
-import { invoke } from "@tauri-apps/api/core";
+import { convertFileSrc } from '@tauri-apps/api/core';
+import { invoke } from '@tauri-apps/api/core';
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
+let viewerEl: HTMLImageElement | null = null;
+let statusEl: HTMLParagraphElement | null = null;
 
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
+async function showLaunchImage() {
+  //rustのcommandから画像パスの入手
+  const path = await invoke<string | null>('get_launch_image_path');
+
+  if (!viewerEl || !statusEl) return;
+
+  if (path && path.length > 0) {
+    //ローカルファイル画像のURL変換
+    const url = convertFileSrc(path);
+    viewerEl.src = url;
+    statusEl.textContent = `表示中の画像パス: ${path}`;
+  }else {
+    statusEl.textContent = '表示する画像がありません。';
   }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+window.addEventListener('DOMContentLoaded', () => {
+  viewerEl = document.querySelector('#viewer');
+  statusEl = document.querySelector('#status');
+  showLaunchImage();
 });
